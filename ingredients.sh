4abe22a5c3c338e90440bebf77909448cd0ddc4a
Linda Olsson <lindaolsson80@gmail.com>
2020-11-02 23:13:24 +0100

    Newest version of scraper

--- ingredients.sh
+++ ingredients.sh
@@ -14,11 +14,11 @@ do
 	FILENAME=./recept/$(sed "${line}q;d" slugs.tmp | sed s/"'"//g | sed s/'"'//g | sed s/":slug="//g)
 	TITLE=$(sed "${line}q;d" titles.tmp)
 	touch "${FILENAME}"
-	echo -e "${TITLE}\n\n" > "${FILENAME}"
+	echo -e "${TITLE}\n\n" | sed s/':title="'// | sed s/'"$'// > "${FILENAME}"
 
 	slug=$(sed "${line}q;d" slugs.tmp | sed s/"'"//g | sed s/'"'//g | sed s/":slug="//g )
 	echo $slug
-	curl -s "https://recept.se/recept/$slug" | grep ',"recipeIngredient":\[' | sed  -r 's/.*,"recipeIngredient":\[([^]]*)\].*/\1/' | awk  'BEGIN{ FS = "\",\"" } { for (i=1;i<=NF;i++) { print $i }}' | sed 's/"//' >> "${FILENAME}"
+	curl -s "https://recept.se/recept/$slug" | grep ',"recipeIngredient":\[' | sed  -r 's/.*,"recipeIngredient":\[([^]]*)\].*/\1/' | awk  'BEGIN{ FS = "\",\"" } { for (i=1;i<=NF;i++) { print $i }}' | sed 's/"//' | sed s/'\\u00e5'/"å"/g | sed s/'\\u00f6'/"ö"/g | sed s/'\\u00e4'/"ä"/g | sed s/'\u00e9'/"é"/g | sed s/'\u2019'/"'"/g >> "${FILENAME}"
 	echo -e "\nINSTRUCTIONS\n" >> "${FILENAME}"
 	INSTRUCTIONS_JSON=$(curl "https://recept.se/recept/$slug" 2> /dev/null | grep -o "^.*instructions=.*$" | sed s/':instructions='// | sed s/'&quot;'/'"'/g | sed s/'^"'/""/ | sed s/'"$'/""/ )
 	echo "$INSTRUCTIONS_JSON" | jq .[].instructions | jq .[].instruction > tmp.tmp
