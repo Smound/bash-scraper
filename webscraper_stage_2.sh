85c0087e3fe29874bb1738fc661d75d938d034db
Ecce <trollkarln@protonmail.com>
2020-11-16 19:39:05 +0100

    cleaning_code

--- webscraper_stage_2.sh
+++ webscraper_stage_2.sh
@@ -18,10 +18,23 @@ do
 
 	slug=$(sed "${line}q;d" slugs.tmp | sed s/"'"//g | sed s/'"'//g | sed s/":slug="//g )
 	echo $slug
-	curl -s "https://recept.se/recept/$slug" | grep ',"recipeIngredient":\[' | sed  -r 's/.*,"recipeIngredient":\[([^]]*)\].*/\1/' | awk  'BEGIN{ FS = "\",\"" } { for (i=1;i<=NF;i++) { print $i }}' | sed 's/"//' | sed s/'\\u00e5'/"å"/g | sed s/'\\u00f6'/"ö"/g | sed s/'\\u00e4'/"ä"/g | sed s/'\u00e9'/"é"/g | sed s/'\u2019'/"'"/g  | sed s/'\\u00e2'/'á'/g | sed s/'\\u00bd'/'1\/2'/g >> "${FILENAME}"
-	echo -e "\nINSTRUCTIONS\n" >> "${FILENAME}"
+	curl -s "https://recept.se/recept/$slug" |
+	       	grep ',"recipeIngredient":\[' |
+	       	sed  -r 's/.*,"recipeIngredient":\[([^]]*)\].*/\1/' |
+	       	awk  'BEGIN{ FS = "\",\"" } { for (i=1;i<=NF;i++) { print $i }}' |
+	       	sed 's/"//' |
+	       	sed 's/\\u00e5/å/g' |
+	       	sed 's/\\u00f6/ö/g' |
+	       	sed 's/\\u00e4/ä/g' |
+	       	sed 's/\u00e9/é/g' |
+	       	sed 's/\\u00e2/á/g' >> "${FILENAME}"
+	
+echo -e "\nINSTRUCTIONS\n" >> "${FILENAME}"
+
+
 	INSTRUCTIONS_JSON=$(curl "https://recept.se/recept/$slug" 2> /dev/null | grep -o "^.*instructions=.*$" | sed s/':instructions='// | sed s/'&quot;'/'"'/g | sed s/'^"'/""/ | sed s/'"$'/""/ )
 	echo "$INSTRUCTIONS_JSON" | jq .[].instructions | jq .[].instruction > tmp.tmp
+	
 	while read line
 	do
 	    echo -e "$line\n\n" | sed s/'^"'// | sed s/'"$'// >> "${FILENAME}"
