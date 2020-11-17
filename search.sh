e07db7cb33971d653e2ac09780373227152f4e0f
gg <none>
2020-11-17 21:16:18 +0100

    Fixed some more docs, some indentation

--- search.sh
+++ search.sh
@@ -15,7 +15,11 @@
 #                  this mode for "ägg", "mjölk", "socker" or any other
 #                  very common ingredient).
 #
-# This script accepts regexps as query strings.
+# This script accepts regexps as query strings. One example of why
+# regexps are needed, is the following: if you search for "ägg",
+# you'll get also recipes that do not contain "äggs", but that contain
+# the word "lägg". If you search for "\\Wägg\\W" you get only results
+# containing the word "ägg".
 #
 
 PS3="Välj vad du vill söka efter:"
@@ -33,67 +37,68 @@ do
   echo -e "\nDu valde $menu ($REPLY)"
   echo -e "\n-------------------"
 
-if [ $menu == Recept ]; then
-  #statements
-  echo "Skriv in ett recept!"
-  read varstarters
-  echo "Söker efter $varstarters"
-  # find  -iname "$varstarters*"
-  for FILE in $(ls recept)
-  do
-      echo "$FILE":"$(head -1 recept/$FILE)"
-  done | grep -iE "$varstarters" > search.tmp
-
-  IFS="
+  if [ $menu == Recept ]; then
+      #statements
+      echo "Skriv in ett recept!"
+      read varstarters
+      echo "Söker efter $varstarters"
+      # find  -iname "$varstarters*"
+      for FILE in $(ls recept)
+      do
+	  echo "$FILE":"$(head -1 recept/$FILE)"
+      done | grep -iE "$varstarters" > search.tmp
+      
+      IFS="
 "
-  PS3="Vilken recept vill du se? "
-  select RECEPT in $(for line in "$(cat search.tmp)"
-		     do
-			 echo "$line" | cut -d":" -f2
-		     done)
-  do
-      FILENAME=$(cat search.tmp | grep "$RECEPT" | cut -d":" -f1)
-      echo -e "\n\n==========\n"
-      cat recept/$FILENAME
-      echo -e "\n\n"
-      break
-  done
-
-else
-  echo "Skriv in en Ingrediens!"
-  read varstarters
-  
-  echo "Söker efter $varstarters"
-  for FILE in $(ls recept)
-  do
-      LINES_NR=$(wc -l "recept/$FILE")
-      INSTRUCTION_LINE=$(grep -nE "^INSTRUCTIONS" "recept/$FILE" | cut -d ":" -f 1)
-      let "INSTRUCTION_LINE -= 1"
-      cat recept/"$FILE" | head -n ${INSTRUCTION_LINE} | grep -iE "$varstarters" 2>&1 > /dev/null
-      if [ $? -eq 0 ]
-      then
-	  echo -n "$FILE:" >> search.tmp
-	  echo "$(head -1 recept/$FILE)" >> search.tmp
-      fi
-  done
-  
-  IFS="
-"
-  PS3="Vilken recept vill du se? "
-  select RECEPT in $(for line in "$(cat search.tmp)"
-		     do
-			 echo "$line" | cut -d":" -f2
-		     done)
-  do
-      FILENAME=$(cat search.tmp | grep "$RECEPT" | cut -d":" -f1)
-      echo -e "\n\n==========\n"
-      cat recept/$FILENAME
-      echo -e "\n\n"
-      break
-  done
-
-fi
+      PS3="Vilken recept vill du se? "
+      select RECEPT in $(for line in "$(cat search.tmp)"
+			 do
+			     echo "$line" | cut -d":" -f2
+			 done)
+      do
+	  FILENAME=$(cat search.tmp | grep "$RECEPT" | cut -d":" -f1)
+	  echo -e "\n\n==========\n"
+	  cat recept/$FILENAME
+	  echo -e "\n\n"
+	  break
+      done
+      
+  else
+      echo "Skriv in en Ingrediens!"
+      read varstarters
+      
+      echo "Söker efter $varstarters"
+      for FILE in $(ls recept)
+      do
+	  LINES_NR=$(wc -l "recept/$FILE")
+	  INSTRUCTION_LINE=$(grep -nE "^INSTRUCTIONS" "recept/$FILE" | cut -d ":" -f 1)
+	  let "INSTRUCTION_LINE -= 1"
+	  cat recept/"$FILE" | head -n ${INSTRUCTION_LINE} | grep -iE "$varstarters" 2>&1 > /dev/null
+	  if [ $? -eq 0 ]
+	  then
+	      echo -n "$FILE:" >> search.tmp
+	      echo "$(head -1 recept/$FILE)" >> search.tmp
+	  fi
+      done
+      
+      IFS="
+"     
+      PS3="Vilken recept vill du se? "
+      select RECEPT in $(for line in "$(cat search.tmp)"
+			 do
+			     echo "$line" | cut -d":" -f2
+			 done)
+      do
+	  FILENAME=$(cat search.tmp | grep "$RECEPT" | cut -d":" -f1)
+	  echo -e "\n\n==========\n"
+	  cat recept/$FILENAME
+	  echo -e "\n\n"
+	  break
+      done
+      
+  fi
   break;
 done
 
+# Cleanup temporary file
 rm search.tmp
--- webscraper_stage_2.sh
+++ webscraper_stage_2.sh
@@ -9,48 +9,59 @@
 # Recipes will end up in textfiles under the directory ./recept/
 #
 
-grep "^:slug" result.txt > slugs.tmp
-grep "^:title" result.txt > titles.tmp
+grep "^:slug" result.txt > slugs.tmp # Isolate all slugs to a separate file
+grep "^:title" result.txt > titles.tmp # Isolate all titles to a separate file
 
-#Creates a folder "recept"
+# recept will contain all the recipes, each in a textfile
 mkdir recept
 
-#:slug example: surdegsbrod-med-dinkel
+# :slug example: surdegsbrod-med-dinkel
 echo "Grep found $(wc -l slugs.tmp) slugs"
 
-#Prints out number of total recipes
+# Saves number of total recipes to a variable. By design, `wc -l titles.tmp` will return the same
+# number.
 TOTAL_RECIPES=$(wc -l slugs.tmp | cut -d" " -f 1)
 
 
-#Loops through each recipe from $slugs.tmp
-
+# Loops through each recipe from $slugs.tmp and titles.tmp
+#
+# This works because titles.tmp and slugs.tmp have the same order (see comments on webscraper stage
+# 1 for more info).
+#
+# the $line loop-variable is the line number to fetch from each tmp file.
+#
+# Note: `sed "$Nq;d" $FILE` returns only line nr. $N from $FILE. So we use it inside the next for
+#       loop to get line nr. $line on each iteration from both slugs.tmp and titles.tmp.
 for line in $(seq 1 ${TOTAL_RECIPES})
 do
 
-	#Sets the title of the recipe as filename
+	# Sets the slug of the recipe as filename
 	FILENAME=./recept/$(sed "${line}q;d" slugs.tmp | sed s/"'"//g | sed s/'"'//g | sed s/":slug="//g)
 	TITLE=$(sed "${line}q;d" titles.tmp)
 	touch "${FILENAME}"
+	# Write the title to $FILENAME, and a separator for the ingredients' list
 	echo -e "${TITLE}" | sed s/':title="'// | sed s/'"$'// > "${FILENAME}"
 	echo -e "===INGREDIENTS===\n" >> "${FILENAME}"
-
+	# Remove quotation marks (both " and ') from our slug, as well as the prefix ':slug='
 	slug=$(sed "${line}q;d" slugs.tmp | sed s/"'"//g | sed s/'"'//g | sed s/":slug="//g )
+	# Printing the slug we're processing for debugging
 	echo $slug
 
-	#Curls and greps microdata in Json from the $slug
+	# Curls microdata in Json from the $slug, and greps it to remove unneeded tags
 	jsonData=$(curl -s "https://recept.se/recept/$slug" | grep -o '<script type="application/ld+json">.*</script>' | grep -o '\{.*\}')
+	# Changing IFS to easily parse by line instead of by word
 	IFS="
 "
-	#For each line it fetches the ingredients data ,formats it and write it to the file
+	# For each line it fetches the ingredients data ,formats it with the help of `jq` and write it to $FILENAME
 	for LINE in $(echo "$jsonData" | jq .recipeIngredient | jq .[]); do echo $LINE | sed s/'"'//g >> "${FILENAME}"; done
 	
 	echo -e "\n===INSTRUCTIONS===\n" >> "${FILENAME}"
 	
-	#For each line it fetches the instructions data ,formats it and write it to the file
+	# For each line it fetches the instructions data ,formats it with the help of `jq` and write it to $FILENAME
 	for LINE in $(echo "$jsonData" | jq .recipeInstructions.itemListElement | jq .[]); do echo $LINE | sed s/'"'//g >> "${FILENAME}"; done
 
-	
 done
 
+# Cleanup temporary files
 rm slugs.tmp titles.tmp
 
