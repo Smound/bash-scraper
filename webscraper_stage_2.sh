e301138b55ae6b8789895f867f4d47184c2b11fa
gg <none>
2020-11-17 13:55:14 +0100

    First iteration of using *only* json data, incomplete

--- webscraper_stage_2.sh
+++ webscraper_stage_2.sh
@@ -18,27 +18,10 @@ do
 
 	slug=$(sed "${line}q;d" slugs.tmp | sed s/"'"//g | sed s/'"'//g | sed s/":slug="//g )
 	echo $slug
-	curl -s "https://recept.se/recept/$slug" |
-	       	grep ',"recipeIngredient":\[' |
-	       	sed  -r 's/.*,"recipeIngredient":\[([^]]*)\].*/\1/' |
-	       	awk  'BEGIN{ FS = "\",\"" } { for (i=1;i<=NF;i++) { print $i }}' |
-	       	sed 's/"//' |
-	       	sed 's/\\u00e5/å/g' |
-	       	sed 's/\\u00f6/ö/g' |
-	       	sed 's/\\u00e4/ä/g' |
-	       	sed 's/\u00e9/é/g' |
-	       	sed 's/\\u00e2/á/g' >> "${FILENAME}"
-	
-echo -e "\nINSTRUCTIONS\n" >> "${FILENAME}"
+	jsonData=$(curl -s "https://recept.se/recept/$slug" | grep -o '<script type="application/ld+json">.*</script>' | grep -o '\{.*\}')
+	echo "$jsonData" | jq
 
-
-	INSTRUCTIONS_JSON=$(curl "https://recept.se/recept/$slug" 2> /dev/null | grep -o "^.*instructions=.*$" | sed s/':instructions='// | sed s/'&quot;'/'"'/g | sed s/'^"'/""/ | sed s/'"$'/""/ )
-	echo "$INSTRUCTIONS_JSON" | jq .[].instructions | jq .[].instruction > tmp.tmp
-	
-	while read line
-	do
-	    echo -e "$line\n\n" | sed s/'^"'// | sed s/'"$'// >> "${FILENAME}"
-	done < tmp.tmp
+	# TODO: format ingredients and instructions, and write to $FILENAME
 done
 
 rm slugs.tmp titles.tmp tmp.tmp
